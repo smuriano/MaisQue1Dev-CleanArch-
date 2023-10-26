4092afe8b16a445416b6d61b17e513f44ddc47df
Sidney Muriano <smuriano@gmail.com>
2023-10-26 17:50:03 -0300

    feat: initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+# MaisQue1Dev Clean Arch
+Basic project using Clean Arch and Minimal API's to implement business solutions
---
+++ src/MaisQue1Dev.Infra/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace MaisQue1Dev.Infra;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+    public static readonly Assembly GetExecutingAssembly = Assembly.GetExecutingAssembly();
+}
